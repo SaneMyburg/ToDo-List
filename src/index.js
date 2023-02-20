693e2c0b25ad69d8e26fae35046e24553b616e7f
SaneMyburg <sane.myburg@gmail.com>
2023-02-20 08:21:49 +0200

    Dynamically add to do list tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './style.css';
+
+const todoArr = [
+  {
+    index: 1,
+    description: 'wash the dishes',
+    completed: false
+  },
+  {
+    index: 1,
+    description: 'complete To Do list project',
+    completed: false
+  }
+];
+
+const todoTask = document.querySelector('.to-do-task');
+todoArr.forEach((task) => {
+  const div = document.createElement('div');
+  div.classList.add('todo');
+  div.innerHTML += `
+  <input type="checkbox">
+  <p class="">${task.description}</p>
+  <i class="uil uil-ellipsis-v"></i>
+  
+  `;
+  todoTask.append(div);
+});
